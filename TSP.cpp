c9144dc4915be512dda47fbe5c8722b4b6400d1d
rosenauk <56204294+rosenauk@users.noreply.github.com>
2020-03-10 15:56:32 -0700

    Update TSP.cpp

--- TSP.cpp
+++ TSP.cpp
@@ -10,8 +10,8 @@ using namespace std;
 int main()
 {
     ifstream in;
-    ofstream out("tsp_example_1.txt.tour");
-    in.open("tsp_example_1.txt");
+    ofstream out("test-input-1.txt.tour");
+    in.open("test-input-1.txt");
 
     int trash;
     int size;
@@ -35,8 +35,14 @@ int main()
         }
     }
 
+    for (int i = 0; i < size; i++)
+    {
+        visited[i] = 0;
+    }
+
+    int totalDis = 0;
     int current = 0;
-    visited[0] = 0;
+    //visited[0] = 0;
 
     for (int j = 1; j < size; j++)
     {
@@ -47,22 +53,52 @@ int main()
             //distance formula for current node to all other nodes
 
             int dis = 99999;
+            bool valid = true;
             if (k != current)
             {
-                //dist uses unvisited 'current' x and y and unvisited 'k' x and y
-                dis = sqrt(pow(x[current] - x[k], 2) + pow(y[current] - y[k], 2));
-                if (dis < shortest)
+                for (int l = 0; l < size; l++)
                 {
-                    shortest = dis;
-                    shortNode = k;
+                    if (unvisited[k] == visited[l])
+                    {
+                        valid = false;
+                    }
                 }
-            }
 
+                if (valid == true)
+                {
+                    //dist uses unvisited 'current' x and y and unvisited 'k' x and y
+                    dis = sqrt(pow(x[current] - x[k], 2) + pow(y[current] - y[k], 2));
+                    if (dis < shortest)
+                    {
+                        shortest = dis;
+                        shortNode = k;
+                    }
+                }
+                
+            }
 
         }
 
         visited[j] = shortNode;
+        totalDis = totalDis + shortNode;
 
     }
+
+    out << totalDis;
+    out << endl;
+
+    for (int m = 0; m < size; m++)
+    {
+        out << visited[m];
+        out << endl;
+    }
+
+    cout << "Tour completed \n";
+
+    delete[] unvisited;
+    delete[] visited;
+    delete[] x;
+    delete[] y;
+
 }
 
